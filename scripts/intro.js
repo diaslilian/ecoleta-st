7c06ebbe1b3c747989cce84b0ffd9354bbed4f00
diaslilian <lilian.dias25@gmail.com>
2020-06-08 11:45:29 -0300

    config create point events  javascript

---
+++ scripts/intro.js
@@ -0,0 +1,78 @@
+// document.write("Hello")
+
+//variaveis, tipos de dados
+
+// var myvar
+// myvar = "Hello"
+
+// document.write(myvar)
+
+//let myvar = "He"
+//const constvar = "He"
+
+//document.write(myvar + constvar)
+
+//string
+
+//"isso é uma string"
+//'isso também é string'
+//`outra string`
+
+//number
+//const n1 = 1
+//const n2 = 12
+//document.write(n1 + n2)
+
+//boolean (true or false)
+//const btrue = true
+//const bfalse = false
+
+//objeto possuem propriedades e funcionalidades
+//const pessoa = {
+//  altura: "1,80m",
+//  idade: 24,
+//  solteiro: true,
+//função
+//  correr() {
+//    return "run forest"
+//  }
+//}
+
+//document.write(pessoa.correr())
+
+//Array - vetores é uma coleção de dados
+//const colecaoDeBolinhas = ["blue", "green", 1, {name: "my name"}]
+
+//document.write(colecaoDeBolinhas[3].name)
+
+//Funções - funcionalidades - atalhos - reuso de código
+
+//function sayMyName() {
+//  document.write("Lilian")
+//}
+
+//sayMyName()
+
+//reuso de código
+//function sayMyName(name) {
+//  document.write(name)
+//}
+
+//sayMyName("João")
+//sayMyName("Juliana")
+//sayMyName("Lucas")
+
+//condicionais
+
+//const notaFinal = 7
+
+//if(notaFinal < 5) {
+//  document.write("Reprovado")
+//} else {
+//  document.write("Aprovado")
+//}
+
+//loop repetição
+//for (i = 0; i < 10; i++) {
+//  document.write(`<p>${i}</p>`)
+//}
