5640cff9f4345ee2341df905598e1962902702af
diaslilian <lilian.dias25@gmail.com>
2020-06-23 21:30:44 -0300

    config nunjucks and updating of page structure

--- src/server.js
+++ src/server.js
@@ -4,18 +4,25 @@ const server = express();
 // configurar pasta public
 server.use(express.static("public"));
 
+// utilizando template engine (nunjucks)
+const nunjucks = require("nunjucks");
+nunjucks.configure("src/views", {
+  express: server,
+  noCache: true,
+});
+
 // caminhos da aplicação
 //página inicial
 server.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/index.html");
+  return res.render("index.html");
 });
 
 server.get("/create-point", (req, res) => {
-  res.sendFile(__dirname + "/views/create-point.html");
+  return res.render("create-point.html");
 });
 
 server.get("/search-results", (req, res) => {
-  res.sendFile(__dirname + "/views/search-results.html");
+  return res.render("search-results.html");
 });
 
 // ligar o servidor
